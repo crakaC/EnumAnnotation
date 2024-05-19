78d6156c3faabd240cc78bd1ac49b4317fb4dcf9
Kosuke SHIRAKASHI <k.shira86@gmail.com>
2024-05-19 22:57:54 +0900

    publishing

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,9 +1,11 @@
 import org.jetbrains.dokka.DokkaConfiguration.Visibility
 import org.jetbrains.dokka.gradle.DokkaTask
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.dokka)
+    id("maven-publish")
 }
 
 dependencies {
@@ -11,14 +13,55 @@ dependencies {
     implementation(libs.kotlinPoet)
 }
 
+val dokkaHtml by tasks.getting(DokkaTask::class)
+
 tasks.dokkaHtml.configure {
-    outputDirectory.set(rootProject.layout.buildDirectory.file("docs").get().asFile)
+    outputDirectory.set(project.layout.buildDirectory.file("docs").get().asFile)
 }
 
 tasks.withType<DokkaTask> {
     dokkaSourceSets {
         configureEach {
             documentedVisibilities = setOf(Visibility.PUBLIC)
+        }
+    }
+}
+
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    dependsOn(dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+publishing {
+    val properties = Properties().apply {
+        val credentialFile = project.layout.projectDirectory.file("credentials.properties").asFile
+        if (credentialFile.exists()) {
+            credentialFile.inputStream().use { load(it) }
+        }
+    }
+    val packagingUser = properties["gpr.user"] as? String ?: System.getenv("PACKAGING_USER")
+    val packagingToken = properties["gpr.token"] as? String ?: System.getenv("PACKAGING_TOKEN")
+
+    publications {
+        register<MavenPublication>("gpr") {
+            groupId = "com.crakac"
+            artifactId = "parsableenum"
+            version = System.getenv("RELEASE_VERSION") ?: "0.1.0-SNAPSHOT"
+            from(components["kotlin"])
+            artifact(tasks.kotlinSourcesJar)
+            artifact(javadocJar)
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/crakac/ParsableEnum")
+            credentials {
+                username = packagingUser
+                password = packagingToken
+            }
         }
     }
 }
